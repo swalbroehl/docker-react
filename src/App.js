9edc2418f6a3aa27887ad9974a18704f9baeae06
root <scienceandfiction@gmail.com>
2019-02-18 08:11:24 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>Hello</code> and save to reload.
+            Edit <code>I was changed on the feature branch</code> and save to reload.
           </p>
           <a
             className="App-link"
